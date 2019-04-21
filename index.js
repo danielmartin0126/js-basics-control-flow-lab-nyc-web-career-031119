f6a859ea27b5754b70c557643277126901f01bc2
Danny Martin <danielmartin0126@gmail.com>
2019-04-21 21:34:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ function scuberGreetingForFeet(ride) {
   if (ride < 400) {
     'This one is on me!'
   }
-  elsif (ride <)
+  elsif (ride > 2000) {
+    'I will gladly take your thirty bucks.'
+  }
   
 }
